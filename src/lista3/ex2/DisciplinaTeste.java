3494ce33fc52b290570b6300fcb884d9ab2a3d45
Lucas Gabriel Von Ryn <lvonryn@gmail.com>
2024-04-21 17:41:28 -0300

    Add files via upload

---
+++ src/lista3/ex2/DisciplinaTeste.java
@@ -0,0 +1,11 @@
+package lista3.ex2;
+
+public class DisciplinaTeste {
+    
+    public static void main(String[] args) {
+
+        Disciplina disciplina1 = new Disciplina(1, "Matemática 1", "Presencial", 36, "Funções");
+
+        disciplina1.emitirRelatorio();
+    }
+}
