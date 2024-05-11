0c76ad3783da719c950ffaec39fc3fcad7c18184
Lucas Gabriel Von Ryn <lvonryn@gmail.com>
2024-05-11 12:12:08 -0300

    Teste

--- src/aula12/ArrayListTeste.java
+++ src/aula12/ArrayListTeste.java
@@ -1,5 +1,20 @@
 package aula12;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArrayListTeste {
 
+    public static void main(String[] args) {
+          
+        List listaGenerica = new ArrayList();
+
+        listaGenerica.add("Lucas Von Ryn");
+        listaGenerica.add("Milena Ferreira");
+
+        for(int i = 0; i < listaGenerica.size(); i++) {
+
+            System.out.println(listaGenerica.get(i));
+        }
+    }
 }
